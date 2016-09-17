81ae45388d0f95d6ca0ad68607c4426e40da4e42
lenin fernando <ingeleninfernando@gmail.com>
2016-09-17 13:38:03 -0500

    'v1'

---
+++ app/controllers/api/v1/products_controller.rb
@@ -0,0 +1,45 @@
+module Api
+  module V1
+    class ProductsController < ApplicationController
+
+
+      def index
+        @products=Product.all
+        respond_to do |format|
+            format.json {render :json => @products}
+          end
+      end
+
+      def create
+        product=Product.new(product_params)
+        if product.save
+          render json: product, status: 201
+        else
+          render json: {errors: product.errors}, status: 422
+        end
+      end
+
+      def update
+         product = Product.find(params[:id])
+        if product.update(product_params)
+          render json: product, status: 200
+        else
+          render json: {errors: product.errors}, status: 422
+        end
+      end
+
+      def destroy
+        product =Product.find(params[:id])
+        if product.destroy
+          render json: {}, status: 204
+        end
+      end
+
+      private
+      def product_params
+        params.require(:product).permit(:name, :price)
+      end
+
+    end
+  end
+end
